2d4b211dd8f03dfcf9f8013e0dc0917a2df3f930
RedFreeEZ <91824654+RedFreeEZ@users.noreply.github.com>
2022-10-12 17:46:27 +0300

    Машина кать туда-сюда выдавать через сколько приедет UwU

---
+++ src/com/company/CAR.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public class CAR implements Runnable {
+    private int min = 10;
+    private int max = 80;
+    private int km;
+    private int time = 0;
+    private int speed = 0;
+    private int sr = 0;
+    private int speedForM = 0;
+
+    private Random randomSpeed = new Random();
+
+    public CAR(int km) {
+        this.km = km * 1000;
+    }
+
+    public int getkm() {
+        return km;
+    }
+
+    @Override
+    public void run() {
+        while (km > 0) {
+            speed = randomSpeed.nextInt(max - min) + min;
+            km = km - speed;
+            time = time + 1;
+            speedForM = speedForM + speed;
+            sr = speedForM / time;
+
+            double ost = km / sr;
+
+            //   System.out.println("—редн€€ скорость "+sr);
+
+
+            System.out.println("ќсталось: " + getkm() + "м" + " —корость: " + speed + " ¬рем€: " + time + " —редн€€ скорость: " + sr + " ¬рем€ прибыти€: " + ost);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,34 +6,8 @@ import java.util.concurrent.FutureTask;
 public class Main {
     private int arrsp[] = new int[5];
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        // FutureTask is a concrete class that
-        // implements both Runnable and Future
-        // Future как об объекте, который хранит результат - он может не удерживать его пр€мо сейчас, но будет делать это в будущем (как только Callable вернетс€). “аким образом, Future - это, по сути, один из способов, с помощью которого основной поток может отслеживать прогресс и результаты других потоков.
-        FutureTask[] randomNumberTasks = new FutureTask[5];
+        CAR car = new CAR(1);
+        car.run();
 
-        for (int i = 0; i < 5; i++)
-        {
-            CallableExample callable = new CallableExample();
-
-            // Create the FutureTask with Callable
-            randomNumberTasks[i] = new FutureTask(callable);
-
-            // As it implements Runnable, create Thread
-            // with FutureTask
-            Thread t = new Thread(randomNumberTasks[i]);
-            t.start();
-
-        }
-        for (int i = 0; i < 5; i++)
-        {
-            // As it implements Future, we can call get()
-            System.out.println(randomNumberTasks[i].get());
-            // Ётот метод блокируетс€ до тех пор, пока не будет получен результат
-            // ћетод get может генерировать проверенные исключени€
-            // нравитс€, когда его прерывают. Ёто и есть причина
-            // дл€ добавлени€ предложени€ throws в main
-
-
-        }
     }
 }
